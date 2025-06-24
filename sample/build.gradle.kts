c198bcc8b0d7feb3e5d7fdf6cf4fdeacba08676a
86156 <wc0811@163.com>
2025-06-25 06:39:14 +0800

    Add test

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(project(":library"))
+//    implementation(project(":library"))
+    implementation(libs.cwlib)
 
 }
